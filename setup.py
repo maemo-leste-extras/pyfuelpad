33581d9564339498c0691136e58a1627b654b8bb
Javier Palacios <javiplx@gmail.com>
2010-06-07 13:09:14 +0200

    Added distutil script and extra files

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name = 'pyfuelpad',
+    version = '1.0',
+    description = 'Fuelpad reimplementation',
+    author = 'Javier Palacios',
+    author_email = 'javiplx@gmail.com',
+    license = 'GPLv2',
+    url = 'http://gitorious.org/pyfuelpad/pyfuelpad',
+    scripts = [ 'pyfuelpad' ],
+    data_files = [
+                 ( 'share/applications/hildon' , [ 'pyfuelpad.desktop' ] ) ,
+                 ( 'share/icons/hicolor/scalable/hildon' , [ 'icons/scalable/pyfuelpad.png' ] )
+                 ],
+    py_modules = [ 'callbacks' , 'combos' , 'configuration' , 'db'  , 'ui' , 'utils' ]
+    )
+
